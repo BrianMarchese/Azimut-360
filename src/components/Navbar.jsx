845f27a9b32173aa99e46ab22f2ad4defe245127
BrianMarchese <brianmarchese@hotmail.com>
2026-09-03 16:36:54 -0300

    agregado de hero

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -15,7 +15,7 @@ export const Navbar = () => {
   ];
 
   return (
-    <header className="fixed top-0 left-0 w-full z-50 transition-all duration-300 bg-black/15 backdrop-blur-md border-b border-white/10 font-heading">
+    <header className="fixed top-0 left-0 w-full z-50 transition-all duration-300 bg-black/15 border-white/10 font-heading">
       <div className=" mx-auto px-4 sm:px-6 lg:px-8 py-4">
         <div className="flex items-center justify-between">
           
